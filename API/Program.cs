f7b8017f65bee3efc6149b53ac4a05d21ae382ab
ingarspaeglitis <ingars@standardsdigital.com>
2025-06-29 20:00:50 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,14 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
